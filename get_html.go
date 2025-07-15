75be6baf29ddb2977c3243e6a257b2a4aca63dd0
iamwzl <wzl@wzl.to>
2025-07-15 03:24:21 +0100

    L5&L6

--- get_html.go
+++ get_html.go
@@ -1,59 +1,39 @@
 package main
 
 import (
-	"errors"
-	"log"
-	"net/url"
-	"strings"
-
-	"golang.org/x/net/html"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 )
 
-var ErrInvalidBaseURL = errors.New("invalid or empty base URL provided")
+var httpClient = &http.Client{
+	Timeout: 10 * time.Second,
+}
 
-func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-
-	if len(rawBaseURL) == 0 {
-		return nil, ErrInvalidBaseURL
-	}
-
-	baseURL, err := url.Parse(rawBaseURL)
-	if err != nil || baseURL == nil || len(baseURL.String()) == 0 || !(baseURL.Scheme == "http" || baseURL.Scheme == "https") {
-		return nil, ErrInvalidBaseURL
-	}
-
-	htmlReader := strings.NewReader(htmlBody)
-	htmlNodes, err := html.Parse(htmlReader)
+func getHTML(rawURL string) (string, error) {
+	req, err := http.NewRequest("GET", rawURL, nil)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("request failed: %v", err)
 	}
 
-	var foundHrefs = make([]string, 0)
+	req.Header.Set("User-Agent", "bootdev-bootie-crawler/1.0")
 
-	for thisNode := range htmlNodes.Descendants() {
-
-		if thisNode.Type == html.ElementNode && thisNode.Data == "a" {
-			for _, attr := range thisNode.Attr {
-				if attr.Key == "href" {
-
-					thisURL, err := url.Parse(attr.Val)
-					if err != nil {
-						log.Printf("Error parsing href '%s': %v", attr.Val, err)
-						continue
-					}
-
-					if thisURL.Scheme != "" && thisURL.Scheme != "http" && thisURL.Scheme != "https" {
-						log.Printf("Skipping href with unsupported scheme '%s': %s", thisURL.Scheme, attr.Val)
-						continue
-					}
-
-					foundHrefs = append(foundHrefs, baseURL.ResolveReference(thisURL).String())
-					continue
-				}
-			}
-
-		}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("getting response failed: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return "", fmt.Errorf("non 2XX http status code (%d)", resp.Status)
 	}
 
-	return foundHrefs, nil
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("unable to ReadAll body: %v", err)
+
+	}
+
+	return string(respBody), nil
+
 }
--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	if len(os.Args) <= 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := os.Args[1]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
+
+	gotHTML, err := getHTML(baseURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Print(gotHTML)
 
 }
